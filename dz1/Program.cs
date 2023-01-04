c3024f170e769188e583f5df0d1b4d1e83d520ba
Vazgen <vzgo03@mail.ru>
2023-01-05 00:41:52 +0300

    первое сохранеие

---
+++ dz1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("N:");
+int n = Convert.ToInt32(Console.ReadLine());
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+      Console.WriteLine($"Сумма: {sum}");
+        return;          
+    }
+    sum = sum + (numberM++); GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(m, n, 0);
---
+++ dz2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("N:");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+void NaturalNumbers(int num, int num2)
+{
+    if (num>num2) 
+    {
+        
+        Console.Write($"{num} ");
+        NaturalNumbers(num-1,num2);
+    }
+    else Console.Write($"{num} ");
+}
+
+NaturalNumbers(m,n);
